8ccb12bb1db19c9f90c381144f0f63085a945341
Владислав <106609904+bdwayne11@users.noreply.github.com>
2023-01-29 23:24:23 +0300

    Update settings.py

--- backend/foodgram_backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/foodgram_backend/settings.py
@@ -7,9 +7,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv(key='DJANGO_SECRET_KEY')
 
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['158.160.40.65']
+ALLOWED_HOSTS = ['*', '158.160.40.65']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
